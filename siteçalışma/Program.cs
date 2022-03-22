1f102c43468cf91a89583fe70093a48822e29fd2
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-03-22 14:00:37 +0300

    aa

---
+++ siteçalışma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace siteçalışma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
