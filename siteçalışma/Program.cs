040f726d10fa9a90fa831e4d85d16a7ecae5cfab
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-03-22 14:27:03 +0300

    yüke

--- siteçalışma/Program.cs
+++ siteçalışma/Program.cs
@@ -6,7 +6,34 @@ namespace siteçalışma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string seçim1 = "koşmaya başladın";
+            string seçim2 = "zıpladın";
+            string seçim3 = "uçmayı denedin";
+            string seçim4 = "bölündün ";
+            string seçim5 = "simbiyot budlun";
+
+            Console.WriteLine(seçim1);
+            Console.WriteLine(seçim2);
+            Console.WriteLine(seçim3);
+            Console.WriteLine(seçim4);
+            Console.WriteLine(seçim5);
+
+
+
+
+            string[] kaçışlar = new string[] {"koşmaya başladın","zıpladın","uçmayı denedin","seni yedi" };
+
+
+            for (int i = 0; i < kaçışlar.Length; i++)
+            {
+
+                Console.WriteLine(kaçışlar[i]);
+            }
+
+            Console.WriteLine("seçimler bu kadar");
+
         }
+
+      
     }
 }
