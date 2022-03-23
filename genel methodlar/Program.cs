92f56bb73ed5ff22b2ee211c369ff5d7ffb22673
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-03-23 17:10:31 +0300

    güncelle

---
+++ genel methodlar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace genel_methodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ genel methodlar/listem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace genel_methodlar
+{
+    class listem<T>
+    {
+        T[] itemler;
+        public void Ekle(T item  )
+        {
+                        
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
